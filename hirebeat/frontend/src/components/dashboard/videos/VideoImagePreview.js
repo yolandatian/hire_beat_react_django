49072ad14169f9cfb00807a71cde0a7b326d4b1c
Nibo Ying <nibo.ying@duke.edu>
2020-06-10 17:47:50 -0400

    Make VideoImagePreview a function component

--- hirebeat/frontend/src/components/dashboard/videos/VideoImagePreview.js
+++ hirebeat/frontend/src/components/dashboard/videos/VideoImagePreview.js
@@ -1,54 +1,39 @@
-import React, { Component, Image } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import videoImg from "../../../assets/video.png";
 import ImageButton from "../../basic/ImageButton";
 import ReviewStatusButton from "./ReviewStatusButton";
 import { renderQDes } from "../DashboardComponents";
-import { withRouter } from "react-router-dom";
 
-export class VideoImagePreview extends Component {
+export function VideoImagePreview(props) {
   // control status, render modal
-  static propTypes = {
-    v: PropTypes.object.isRequired,
-  };
-
-  redirectToVideoPlayer = () => {
-    const { history } = this.props;
-    if (history) history.push(`/video/${this.props.v.id}`);
-  };
-
-  render() {
-    return (
-      <div className="height-200">
-        <div className="row">
-          <div className="col-5">
-            <ImageButton src={videoImg} func={this.redirectToVideoPlayer} />
+  return (
+    <div className="height-200">
+      <div className="row">
+        <div className="col-5">
+          <ImageButton src={videoImg} func={() => {}} />
+        </div>
+        <div className="col d-flex flex-column justify-content-start container">
+          <h3 className="height-50">Q:{renderQDes(props.v.q_description)}</h3>
+          <div className="d-flex justify-content-start">
+            <p className="text-secondary">{props.v.q_type}</p>
+            <p
+              className="text-secondary"
+              style={{ marginLeft: "5px", marginRight: "5px" }}
+            >
+              {" | "}
+            </p>
+            <p className="text-secondary">
+              {props.v.created_at.substring(0, 10)}
+            </p>
           </div>
-          <div className="col d-flex flex-column justify-content-start container">
-            <h3 className="height-50">
-              Q:{renderQDes(this.props.v.q_description)}
-            </h3>
-            <div className="d-flex justify-content-start">
-              <p className="text-secondary">{this.props.v.q_type}</p>
-              <p
-                className="text-secondary"
-                style={{ marginLeft: "5px", marginRight: "5px" }}
-              >
-                {" | "}
-              </p>
-              <p className="text-secondary">
-                {this.props.v.created_at.substring(0, 10)}
-              </p>
-            </div>
-            <ReviewStatusButton
-              v={this.props.v}
-              sendVideoForReview={this.props.sendVideoForReview}
-            />
-          </div>
+          <ReviewStatusButton
+            v={props.v}
+            sendVideoForReview={props.sendVideoForReview}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default withRouter(VideoImagePreview);
+export default VideoImagePreview;
