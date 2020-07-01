d6827620b77ed80e06829a8e88e45967ff3a68a1
Nibo Ying <nibo.ying@duke.edu>
2020-07-01 14:43:23 -0400

    Production 4

--- hirebeat/frontend/src/index.js
+++ hirebeat/frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import React from "react";
 import "bootswatch/dist/materia/bootstrap.min.css";
 
+// Disable all console messages in production
+console.log = function () {};
+console.error = function () {};
+console.warn = function () {};
+
 ReactDOM.render(<App />, document.getElementById("app"));
 
 //This actual template, the html file is in frontend/static/frontend
